08d78081f92f9afc4354155691022c7687dcf20d
Kai120789 <zoomer120789@gmail.com>
2024-09-20 08:51:33 +0300

    init

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,21 @@
+package mai
+
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: init server
+
+}
