a7e0411edb7837e3f442898176f9d6471ef06b28
Kai120789 <zoomer120789@gmail.com>
2024-09-20 09:32:50 +0300

    feat: logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
 
 	"github.com/joho/godotenv"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -15,7 +22,10 @@ func main() {
 
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := settupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init logger: slog
 
@@ -24,5 +34,26 @@ func main() {
 	// TODO: init router: chi, "chi render"
 
 	// TODO: init server
+
+}
+
+func settupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 
 }
