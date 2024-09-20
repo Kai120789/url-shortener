0ee814875f5e225f162568a8dd33135525f64beb
Kai120789 <zoomer120789@gmail.com>
2024-09-20 10:33:14 +0300

    feat: init storage

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 
 	"github.com/joho/godotenv"
 )
@@ -22,12 +25,20 @@ func main() {
 
 	cfg := config.MustLoad()
 
+	fmt.Print(cfg)
+
 	log := settupLogger(cfg.Env)
 
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	// TODO: init logger: slog
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	// TODO: init storage: sqlite
 
