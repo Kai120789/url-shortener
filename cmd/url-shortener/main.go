d1db85f2ceefb9d401d9028ad8c631abc1a48428
Kai120789 <zoomer120789@gmail.com>
2024-09-20 09:19:37 +0300

    feat: config

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,11 +1,18 @@
-package mai
+package main
 
 import (
 	"fmt"
+	"log"
 	"url-shortener/internal/config"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load("../../local.env"); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg)
